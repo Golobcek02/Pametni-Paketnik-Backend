593eb624fa4a2421b2ea880145abf760e722f7d5
PainOfExistance <matej113a@gmail.com>
2023-05-08 18:56:51 +0200

    fix for get  entries

--- endpoints/routes.go
+++ endpoints/routes.go
@@ -9,6 +9,6 @@ import (
 func Router(Router *gin.Engine) {
 	Router.POST("/register", controllers.Register)
 	Router.POST("/login", controllers.LogIn)
-	Router.POST("/newEntry", controllers.InsertNewEntry)
+	Router.POST("/newEntry/:id", controllers.InsertNewEntry)
 	Router.GET("/getEntries", controllers.GetUserEntries)
 }
