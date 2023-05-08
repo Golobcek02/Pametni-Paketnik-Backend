03f3c3ae970ffc98cb2b3d418a445dce95c981fd
Golobcek <klemen.golob77@gmail.com>
2023-05-08 17:39:31 +0200

    Changed Database name

--- utils/database.go
+++ utils/database.go
@@ -22,7 +22,7 @@ func CheckBase() *mongo.Client {
 
 // ConnectToBase creates a connection to the MongoDB instance and returns it
 func ConnectToBase() *mongo.Client {
-	newMongoInstance, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://"+url.QueryEscape("drvocepalci")+":"+url.QueryEscape("drvocepalci")+"@cluster0.ioplwkz.mongodb.net/?retryWrites=true&w=majority"))
+	newMongoInstance, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://"+url.QueryEscape("PametniPaketnik")+":"+url.QueryEscape("PametniPaketnik")+"@cluster0.ioplwkz.mongodb.net/?retryWrites=true&w=majority"))
 	if err != nil {
 		log.Printf("(ConnectToMongoDB) There was an error creating the mongoDB mongoDBInstance: %v", err)
 	}
