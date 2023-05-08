26d469f196e6b82bd188d610e2fee0dcef1eb6d4
ReneRajzman <rene.rajzman@student.um.si>
2023-05-08 17:36:27 +0200

    Register implementation

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"backend/schemas"
+	"backend/utils"
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+)
+
+// Register function handles the user registration process.
+func Register(c *gin.Context) {
+	var registerUser schemas.User
+
+	if err := c.BindJSON(&registerUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var result schemas.User
+	res := utils.CheckBase().Database("drvocepalci").Collection("users").FindOne(context.Background(), bson.M{"username": registerUser.Username}).Decode(&result)
+	if res == mongo.ErrNoDocuments {
+		registerUser.Password = utils.Hash(registerUser.Password)
+		result, err := utils.CheckBase().Database("drvocepalci").Collection("users").InsertOne(context.TODO(), registerUser)
+		fmt.Println(err)
+		fmt.Println(result.InsertedID)
+		c.IndentedJSON(http.StatusOK, "Poceede")
+		return
+	}
+
+	c.IndentedJSON(http.StatusBadRequest, "Denied")
+}
--- endpoints/routes.go
+++ endpoints/routes.go
@@ -1,1 +1,12 @@
 package endpoints
+
+import (
+	"backend/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router(Router *gin.Engine) {
+	Router.POST("/register", controllers.Register)
+
+}
