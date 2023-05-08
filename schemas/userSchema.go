7f8f7f33db05475e8cbf4beec0ae9f32141192ad
Golobcek <klemen.golob77@gmail.com>
2023-05-08 17:26:59 +0200

    Created schema for User

--- schemas/userSchema.go
+++ schemas/userSchema.go
@@ -1,1 +1,13 @@
 package schemas
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string
+	Surname   string
+	Username  string
+	Email     string
+	Password  string
+	UserBoxes string
+}
