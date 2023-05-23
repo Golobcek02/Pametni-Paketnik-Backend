1628b8e1ee86fa2a815311e2ae636249c320a4fc
PainOfExistance <matej113a@gmail.com>
2023-05-23 20:49:01 +0200

    modified main so its ready for hosting

--- main/main.go
+++ main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"backend/endpoints"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +10,13 @@ import (
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	Router := gin.Default()
-	Router.Use(cors.Default())
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+	Router.Use(cors.New(config))
 
 	endpoints.Router(Router)
 
-	Router.Run("localhost:5551")
+	Router.Run("0.0.0.0:5551")
 }
