23a115bdd2299887a58ed9e9ab45f91b9c1446cb
Golobcek <klemen.golob77@gmail.com>
2023-05-24 20:11:10 +0200

    ,

--- controllers/packageRoutesController.go
+++ controllers/packageRoutesController.go
@@ -223,7 +223,7 @@ func PopFirstStop(c *gin.Context) {
 			docs = append(docs, entry)
 		}
 
-		if len(docs) == 0 {
+		if len(docs) == 1 {
 			_, err := utils.CheckBase().Database("PametniPaketnik").Collection("entries").InsertMany(context.Background(), docs)
 			if err != nil {
 				panic(err)
