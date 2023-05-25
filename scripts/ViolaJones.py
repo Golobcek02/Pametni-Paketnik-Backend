5830fb9a0d76039e9d70c4d1e9b9092f325d0e93
ReneRajzman <rene.rajzman@student.um.si>
2023-05-25 19:32:09 +0200

    Implemented option for reading and writing from np.array

--- scripts/ViolaJones.py
+++ scripts/ViolaJones.py
@@ -1,53 +1,72 @@
 import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-img = cv2.imread('lucka.jpg')
+def display_images(image_array):
+    for i, image in enumerate(image_array):
+        cv2.imshow(f'Image {i + 1}', image)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-margin = 0.2
+def process_images(image_array):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-def process_face(face):
-    (x, y, w, h) = face
-    margin_x = int(w * margin)
-    margin_y = int(h * margin)
+    margin = 0.2
 
-    x_start = max(0, x - margin_x)
-    y_start = max(0, y - margin_y)
-    x_end = min(img.shape[1], x + w + margin_x)
-    y_end = min(img.shape[0], y + h + margin_y)
+    def process_face(face, img):
+        (x, y, w, h) = face
+        margin_x = int(w * margin)
+        margin_y = int(h * margin)
 
-    face_img = img[y_start:y_end, x_start:x_end].copy()
-    face_img = cv2.resize(face_img, (100, 100), interpolation=cv2.INTER_AREA)
+        x_start = max(0, x - margin_x)
+        y_start = max(0, y - margin_y)
+        x_end = min(img.shape[1], x + w + margin_x)
+        y_end = min(img.shape[0], y + h + margin_y)
 
-    mask = np.zeros(face_img.shape[:2], np.uint8)
+        face_img = img[y_start:y_end, x_start:x_end].copy()
+        face_img = cv2.resize(face_img, (100, 100), interpolation=cv2.INTER_AREA)
 
-    bgdModel = np.zeros((1, 65), np.float64)
-    fgdModel = np.zeros((1, 65), np.float64)
+        mask = np.zeros(face_img.shape[:2], np.uint8)
 
-    smaller_margin_x = margin_x // 4
-    smaller_margin_y = margin_y // 4
-    rect = (smaller_margin_x, smaller_margin_y, face_img.shape[1] - 2 * smaller_margin_x, face_img.shape[0] - 2 * smaller_margin_y)
+        bgdModel = np.zeros((1, 65), np.float64)
+        fgdModel = np.zeros((1, 65), np.float64)
 
-    cv2.grabCut(face_img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+        smaller_margin_x = margin_x // 4
+        smaller_margin_y = margin_y // 4
+        rect = (smaller_margin_x, smaller_margin_y, face_img.shape[1] - 2 * smaller_margin_x,
+                face_img.shape[0] - 2 * smaller_margin_y)
 
-    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+        cv2.grabCut(face_img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
-    face_img = face_img * mask2[:, :, np.newaxis]
+        mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 
-    return face_img
+        face_img = face_img * mask2[:, :, np.newaxis]
 
-face_images = [process_face(face) for face in faces]
+        return face_img
 
-if not face_images:
-    print("No faces found.")
-else:
-    for i, face_img in enumerate(face_images):
-        cv2.imshow(f'Face {i + 1}', face_img)
+    all_face_images = []
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for image_file in image_array:
+        image = cv2.imread(image_file)
+        if image is None:
+            print(f"Unable to read {image_file}. Skipping...")
+            continue
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        face_images = [process_face(face, image) for face in faces]
+
+        if not face_images:
+            print(f"No faces found in {image_file}.")
+        else:
+            all_face_images.extend(face_images)
+
+    return np.array(all_face_images)
+
+
+# Example usage:
+processed_images = process_images(image_files)
+display_images(processed_images)
